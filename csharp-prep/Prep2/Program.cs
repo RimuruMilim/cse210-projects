fa9a43ce6c0256760b574e704e4e362681945c71
RimuruMilim <nathankurns@gmail.com>
2025-04-28 09:15:27 -0600

    prep 2, if statements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Give me your score: ");
+        string Userinput = Console.ReadLine();
+        double score = double.Parse(Userinput);
+        if (score >= 90)
+        {
+            Console.Write("Your grade is an A");
+        }
+        else if (score >= 80)
+        {
+            Console.Write("Your grade is a B");
+        }
+        else if (score >= 70)
+        {
+            Console.Write("Your grade is a C");
+        }
+        else if (score >= 60)
+        {
+            Console.Write("Your grade is a D");
+        }
+        else if (score < 60)
+        {
+            Console.Write("Your grade is an F");
+        }
+        else
+        {
+            Console.Write("That is not an applicable answer");
+        }
     }
 }
