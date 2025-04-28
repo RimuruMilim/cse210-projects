6f987481985a6dec3b41ca390c76b2127365e37e
RimuruMilim <nathankurns@gmail.com>
2025-04-28 09:41:55 -0600

    do while loop prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random rnd = new Random();
+        int randomNumber = rnd.Next(1, 101); // Generates a number from 0 to 99
+        int userGuess = 0;
+        Console.Write("Guess a random number from 1 - 100: ");
+        do
+        {
+            string response = Console.ReadLine();
+            
+            if (int.TryParse(response, out userGuess))
+            {
+                if (userGuess < randomNumber)
+                {
+                    Console.Write("Too low! Try again. ");
+                }
+                else if (userGuess > randomNumber)
+                {
+                    Console.Write("Too high! Try again. ");
+                }
+                else
+                {
+                    Console.Write("Congratulations! You guessed the correct number!");
+                }
+            }
+            else
+            {
+                Console.Write("Please enter a valid number. ");
+            }
+            
+        }while (userGuess != randomNumber);
     }
 }
