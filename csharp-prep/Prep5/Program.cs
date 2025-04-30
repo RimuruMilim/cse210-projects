ead792bc22cdecdb82acefe95c69781630764b24
RimuruMilim <nathankurns@gmail.com>
2025-04-30 09:42:33 -0600

    Function learning

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the program!");
+
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        
+        Console.Write("Please enter your favorite number: ");
+        double number = double.Parse(Console.ReadLine());
+        double square = SquareCalc(number);
+
+        Console.Write($"{name}, the square of your number is {square}.");
+
+    }
+
+    static double SquareCalc(double num)
+    {
+        double square = num * num;
+        return square;
     }
 }
