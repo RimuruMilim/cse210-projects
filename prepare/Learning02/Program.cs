6d421678f3c01331a843ccda9c3356b4cff29a39
RimuruMilim <nathankurns@gmail.com>
2025-05-07 00:23:28 -0600

    Job and Resume Program finished

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,34 @@
 using System;
 
+// A code template for the category of things known as Person. The
+// responsibility of a Person is to hold and display personal information.
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        // Create job 1
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Create job 2
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Create resume and assign data
+        Resume myResume = new Resume();
+        myResume._name = "John Smith";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display the resume
+        myResume.Display();
     }
 }
