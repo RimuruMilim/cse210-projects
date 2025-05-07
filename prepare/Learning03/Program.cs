9eddc92639ac4cd8640ef55ac5841f1aeeb34789
RimuruMilim <nathankurns@gmail.com>
2025-05-06 23:52:44 -0600

    Learning to build a class in c#

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,52 @@
 using System;
 
+// A code template for the category of things known as Person. The
+// responsibility of a Person is to hold and display personal information.
+public class Person
+{
+    // The C# convention is to start member variables with an underscore _
+    public string _givenName = "";
+    public string _familyName = "";
+
+     // A special method, called a constructor that is invoked using the  
+    // new keyword followed by the class name and parentheses.
+    public Person()
+    {
+    }
+
+     // A method that displays the person's full name as used in eastern 
+    // countries or <family name, given name>.
+    public void ShowEasternName()
+    {
+        Console.WriteLine($"{_familyName}, {_givenName}");
+    }
+
+     // A method that displays the person's full name as used in western 
+    // countries or <given name family name>.
+    public void ShowWesternName()
+    {
+        Console.WriteLine($"{_givenName} {_familyName}");
+    }
+}
+
 class Program
 {
+    
+    
     static void Main(string[] args)
     {
+        
         Console.WriteLine("Hello Learning03 World!");
+        Person nut =new Person();
+
+        Console.Write("Enter your given name: ");
+        nut._givenName = Console.ReadLine();
+
+        Console.Write("Enter your family name: ");
+        nut._familyName = Console.ReadLine();
+
+        nut.ShowEasternName();
+
+        nut.ShowWesternName();
     }
 }
